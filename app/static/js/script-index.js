ec7b18d394b331787b7d2e16eacd4cb3391b0201
Andressa-Anthero7 <andressa.anthero7@gmail.com>
2024-10-23 01:18:12 -0300

    edicao front-end pré textos dos card/ def da index na viwew com implementação  de call me bot

--- app/static/js/script-index.js
+++ app/static/js/script-index.js
@@ -6,4 +6,18 @@ $( document ).ready(function() {
             scrollTop: $("#text-copy-lp").offset().top
         }, 500); // Tempo em milissegundos para o scroll (0.5 segundo)
     });
+
+    $(document).on('keyup','#nome, #whatsapp', function(){
+        nome_digitado = $("#nome").val();
+        whatsapp_digitado = $("#whatsapp").val();
+        if(nome_digitado.length >= 3 && whatsapp_digitado.length >= 10){
+            $("#aceito-termos").removeAttr('disabled');
+        }
+    })
+
+    $(document).on('click','#aceito-termos',function(){
+       $("#btn-enviar").removeAttr('disabled');
+    })
+
+    
 });
--- app/views.py
+++ app/views.py
@@ -1,6 +1,41 @@
 from django.shortcuts import render
+import requests
 
+
+from django.shortcuts import render
+import requests
 
 # Create your views here.
 def index(request):
-    return render(request, 'site/index.html')
+    if request.method == 'POST':
+        # Capturar os dados do formulário
+        nome_leads = request.POST.get('nome') 
+        whats_app_leads = request.POST.get('whatsapp')
+
+        whats_app_receptor = "5516993379492"
+        
+        # Montar a mensagem
+        message = f"Olá, Adriana! Voçe recebeu novo leads- Nome:{nome_leads} - WhatsApp:{whats_app_leads}"
+
+        # Sua API Key fornecida pelo CallMeBot
+        api_key = "1271569"  # Substitua pela sua API Key
+
+        # URL da API do CallMeBot (o WhatsApp deve estar no formato internacional)
+        url = f'https://api.callmebot.com/whatsapp.php?phone={whats_app_receptor}&text={message}&apikey={api_key}'
+
+        # Enviar a mensagem via requisição GET
+        response = requests.get(url)
+
+        # Verificar a resposta
+        if response.status_code == 200:
+            print("Mensagem enviada com sucesso!")
+            return render(request, 'site/index.html')
+        else:
+            print("Falha ao enviar a mensagem.")
+            print(f"Status code: {response.status_code}")
+    else:
+        print("GET")
+        return render(request, 'site/index.html')
+
+    
+
