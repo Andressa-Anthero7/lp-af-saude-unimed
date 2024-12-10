daa526a7ecdec7ce4a95d02b05aa3ca5a44b4a5b
Andressa-Anthero7 <andressa.anthero7@gmail.com>
2024-12-10 02:00:29 -0300

    retorno static anterior

--- app/static/js/script-index.js
+++ app/static/js/script-index.js
@@ -1,5 +1,5 @@
 $(document).on('click', '.close', function () {
-    $('body').css('overflow', 'auto');
+    
     $('html, body').animate({
         scrollTop: $("#text-copy-lp").offset().top
     }, 500);
